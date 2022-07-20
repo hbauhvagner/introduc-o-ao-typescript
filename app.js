5c90f7566154a585a97b189ed4562ac7b448b961
hbauhvagner <hbauhvagner@gmail.com>
2022-07-20 15:01:16 -0300

    Utilização do tipo void em funções

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ function somarValoresNumericos(numero1, numero2) {
     return numero1 + numero2;
 }
 console.log(somarValoresNumericos(1, 4));
+function printaValoresNumericos(numero1, numero2) {
+    console.log(numero1 + numero2);
+}
--- app.ts
+++ app.ts
@@ -1,5 +1,9 @@
-function somarValoresNumericos(numero1: number, numero2: number) {
+function somarValoresNumericos(numero1: number, numero2: number): number {
     return numero1 + numero2;
 }
 
 console.log(somarValoresNumericos(1, 4));
+
+function printaValoresNumericos(numero1: number, numero2: number) {
+    console.log(numero1 + numero2);
+}
