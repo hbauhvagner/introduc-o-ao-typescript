0fd69adcb78bcedc9be806681382c46a5e2e559a
hbauhvagner <hbauhvagner@gmail.com>
2022-07-20 15:44:05 -0300

    O tipo unknown

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 "use strict";
-function printaValoresNumericos(numero1, numero2) {
-    console.log(numero1 + numero2);
+let anyEstaDeVolta;
+anyEstaDeVolta = 3;
+anyEstaDeVolta = 'teste';
+anyEstaDeVolta = 5;
+let stringTeste = 'verificar';
+stringTeste = anyEstaDeVolta;
+let unknownValor;
+unknownValor = 3;
+unknownValor = 'opa';
+unknownValor = true;
+unknownValor = 'vai sim';
+let stringTeste2 = 'agora vai';
+if (typeof unknownValor === 'string') {
+    stringTeste2 = unknownValor;
 }
-function somarValoresNumericosETratar(numero1, numero2, callback) {
-    let resultado = numero1 + numero2;
-    return callback(resultado);
-}
-function aoQuadrado(numero) {
-    return numero * numero;
-}
-function dividirPorEleMesmo(numero) {
-    return numero / numero;
-}
-console.log(somarValoresNumericosETratar(1, 3, aoQuadrado));
-console.log(somarValoresNumericosETratar(1, 3, dividirPorEleMesmo));
--- app.ts
+++ app.ts
@@ -1,20 +1,19 @@
-function printaValoresNumericos(numero1: number, numero2: number): void {
-    console.log(numero1 + numero2);
+let anyEstaDeVolta: any;
+anyEstaDeVolta = 3;
+anyEstaDeVolta = 'teste';
+anyEstaDeVolta = 5;
+
+let stringTeste: string = 'verificar';
+stringTeste = anyEstaDeVolta;
+
+let unknownValor: unknown;
+unknownValor = 3;
+unknownValor = 'opa';
+unknownValor = true;
+unknownValor = 'vai sim';
+
+let stringTeste2: string = 'agora vai';
+
+if (typeof unknownValor === 'string') {
+    stringTeste2 = unknownValor;
 }
-
-function somarValoresNumericosETratar(numero1: number, numero2: number, callback: (numero: number) => number): number {
-    let resultado = numero1 + numero2;
-
-    return callback(resultado); 
-}
-
-function aoQuadrado(numero: number): number {
-    return numero * numero;
-}
-
-function dividirPorEleMesmo(numero: number): number {
-    return numero / numero;
-}
-
-console.log(somarValoresNumericosETratar(1, 3, aoQuadrado));
-console.log(somarValoresNumericosETratar(1, 3, dividirPorEleMesmo));
