03021d54dcbfa72639a2af10dde7698aef39dcce
hbauhvagner <hbauhvagner@gmail.com>
2022-07-20 14:55:39 -0300

    Definindo o tipo de retorno de uma função

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 "use strict";
-function somarValores(input1, input2) {
-    if (typeof input1 === 'string' || typeof input2 === 'string') {
-        return input1.toString() + input2.toString();
-    }
-    else {
-        return input1 + input2;
-    }
+function somarValoresNumericos(numero1, numero2) {
+    return numero1 + numero2;
 }
-console.log(somarValores(1, 5));
-console.log('olá', ', tudo bem?');
-console.log('1', 5);
+console.log(somarValoresNumericos(1, 4));
--- app.ts
+++ app.ts
@@ -1,13 +1,5 @@
-type input = number | string;
-
-function somarValores(input1: input, input2: input) {
-    if (typeof input1 === 'string' || typeof input2 === 'string') {
-        return input1.toString() + input2.toString();
-    } else {
-        return input1 + input2;
-    }
+function somarValoresNumericos(numero1: number, numero2: number) {
+    return numero1 + numero2;
 }
 
-console.log(somarValores(1, 5));
-console.log('olá', ', tudo bem?');
-console.log('1', 5);
+console.log(somarValoresNumericos(1, 4));
